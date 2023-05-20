5badd150b4a554897b8cea563031514d63636640
HukuToc2288 <hukutoc2288@yandex.ru>
2023-05-20 16:43:02 +0300

    move dick command to separate class

--- src/ru/hukutoc2288/howtoshitbot/Bot.kt
+++ src/ru/hukutoc2288/howtoshitbot/Bot.kt
@@ -26,6 +26,7 @@ import com.fasterxml.jackson.module.kotlin.KotlinFeature
 import org.telegram.telegrambots.meta.api.methods.send.SendSticker
 import org.telegram.telegrambots.meta.api.objects.User
 import ru.hukutoc2288.howtoshitbot.commands.AnekCommand
+import ru.hukutoc2288.howtoshitbot.commands.DickCommand
 import ru.hukutoc2288.howtoshitbot.commands.KnbCommand
 import ru.hukutoc2288.howtoshitbot.dao.GdDao
 import ru.hukutoc2288.howtoshitbot.entinies.uptime.UptimeResponse
@@ -163,15 +164,7 @@ class Bot : TelegramLongPollingBot() {
                     override fun execute(message: Message, argsLine: String) = getGayOfDay(message, argsLine)
                 }
             )
-            add(
-                object : CommandFunction(
-                    "dick",
-                    "сыграть в игру \"Песюн\"",
-                    arrayOf("песюн")
-                ) {
-                    override fun execute(message: Message, argsLine: String) = measureDick(message, argsLine)
-                }
-            )
+            add(DickCommand)
             // FIXME: 02.12.2022 будет конфликтовать с топом пидоров когда это будет сделано
             add(
                 object : CommandFunction(
@@ -499,86 +492,6 @@ class Bot : TelegramLongPollingBot() {
         Timer().schedule(3000) {
             sendHtmlMessage(chatId, "Ага, нашёл его! Пидор дня сегодня $textMention", message.messageId)
         }
-    }
-
-    private fun measureDick(message: Message, argsLine: String) {
-        val chatId = message.chatId
-        val user = message.from
-        val mention = if (user.userName != null) {
-            "@${user.userName}"
-        } else if (user.lastName != null) {
-            "<a href=\"tg://user?id=${user.id}\">${user.firstName} ${user.lastName}</a>"
-        } else {
-            "<a href=\"tg://user?id=${user.id}\">${user.firstName}</a>"
-        }
-        val dickInfo = GdDao.getDick(chatId, message.from)
-
-        val nowCalendar = GregorianCalendar()
-        val tomorrowCalendar = GregorianCalendar().apply {
-            add(Calendar.DATE, 1)
-        }
-        val nextTimeString =
-            if (nowCalendar.get(Calendar.HOUR_OF_DAY) == 23) {
-                if (nowCalendar.get(Calendar.MINUTE) == 59) {
-                    if (nowCalendar.get(Calendar.SECOND) == 59) {
-                        "сейчас"
-                    } else {
-                        "${60 - tomorrowCalendar.get(Calendar.SECOND)} секунд"
-                    }
-                } else {
-                    "${60 - tomorrowCalendar.get(Calendar.MINUTE)} минут"
-                }
-            } else {
-                "${24 - tomorrowCalendar.get(Calendar.HOUR_OF_DAY)} часов"
-            }
-
-        if (dickInfo == null) {
-            // no dick branch
-            val dickSize = GdDao.getAverageDick(message.chatId) ?: (1..10).random()
-            sendHtmlMessage(
-                chatId,
-                "$mention, теперь у тебя есть песюн в этом чате, и его длина $dickSize см. Продолжай играть через $nextTimeString",
-                message.messageId
-            )
-            GdDao.updateDick(chatId, user, Timestamp(nowCalendar.timeInMillis), dickSize)
-            return
-        }
-        if (DateUtils.isToday(dickInfo.first)) {
-            // already measured branch
-            sendHtmlMessage(
-                chatId,
-                "$mention, ты сегодня уже играл, и длина твоего песюна ${dickInfo.second} см. Продолжай играть через $nextTimeString",
-                message.messageId
-            )
-            return
-        }
-
-        // play branch
-        // песюн не может быть менее 1 см (а почему??)
-        val dickChange = (max(-5, -dickInfo.second + 1)..9).random().let {
-            // ага, 0 это 10
-            if (it == 0)
-                10
-            else
-                it
-        }
-        sendHtmlMessage(
-            chatId,
-            "$mention, твой песюн ${if (dickChange > 0) "вырос на $dickChange" else "скоротился на ${-dickChange}"} см.\n" +
-                    "Теперь его длина ${dickInfo.second + dickChange} см. Продолжай играть через $nextTimeString" +
-                    if (dickInfo.second + dickChange <= KnbCommand.bet &&
-                        KnbCommand.waitingPlayers[chatId]?.first?.id == user.id && KnbCommand.waitingPlayers.remove(
-                            chatId
-                        ) != null
-                    ) {
-                        "\n\nДлина твоего песюна стала меньше ${KnbCommand.bet + 1} см, поэтому ты исключён из игры ${KnbCommand.gameTitle}"
-                    } else {
-                        ""
-                    },
-            message.messageId
-        )
-        GdDao.updateDick(chatId, user, Timestamp(nowCalendar.timeInMillis), dickInfo.second + dickChange)
-        return
     }
 
     private fun showDickTop(message: Message, argsLine: String) {
---
+++ src/ru/hukutoc2288/howtoshitbot/commands/DickCommand.kt
@@ -0,0 +1,96 @@
+package ru.hukutoc2288.howtoshitbot.commands
+
+import org.telegram.telegrambots.meta.api.objects.Message
+import ru.hukutoc2288.howtoshitbot.bot
+import ru.hukutoc2288.howtoshitbot.dao.GdDao
+import ru.hukutoc2288.howtoshitbot.utils.CommandFunction
+import ru.hukutoc2288.howtoshitbot.utils.DateUtils
+import java.sql.Timestamp
+import java.util.*
+import kotlin.math.max
+
+object DickCommand : CommandFunction("dick", "сыграть в игру \"Песюн\"", arrayOf("песюн")) {
+
+    override fun execute(message: Message, argsLine: String) {
+        val start = System.currentTimeMillis()
+        val chatId = message.chatId
+        val user = message.from
+        val mention = if (user.userName != null) {
+            "@${user.userName}"
+        } else if (user.lastName != null) {
+            "<a href=\"tg://user?id=${user.id}\">${user.firstName} ${user.lastName}</a>"
+        } else {
+            "<a href=\"tg://user?id=${user.id}\">${user.firstName}</a>"
+        }
+        val dickInfo = GdDao.getDick(chatId, message.from)
+
+        val nowCalendar = GregorianCalendar()
+        val tomorrowCalendar = GregorianCalendar().apply {
+            add(Calendar.DATE, 1)
+        }
+        val nextTimeString =
+            if (nowCalendar.get(Calendar.HOUR_OF_DAY) == 23) {
+                if (nowCalendar.get(Calendar.MINUTE) == 59) {
+                    if (nowCalendar.get(Calendar.SECOND) == 59) {
+                        "сейчас"
+                    } else {
+                        "${60 - tomorrowCalendar.get(Calendar.SECOND)} секунд"
+                    }
+                } else {
+                    "${60 - tomorrowCalendar.get(Calendar.MINUTE)} минут"
+                }
+            } else {
+                "${24 - tomorrowCalendar.get(Calendar.HOUR_OF_DAY)} часов"
+            }
+
+        if (dickInfo == null) {
+            // no dick branch
+            val dickSize = GdDao.getAverageDick(message.chatId) ?: (1..10).random()
+            bot.sendHtmlMessage(
+                chatId,
+                "$mention, теперь у тебя есть песюн в этом чате, и его длина $dickSize см. Продолжай играть через $nextTimeString",
+                message.messageId
+            )
+            GdDao.updateDick(chatId, user, Timestamp(nowCalendar.timeInMillis), dickSize)
+            return
+        }
+        if (DateUtils.isToday(dickInfo.first)) {
+            // already measured branch
+            println("send :${System.currentTimeMillis()-start}ms")
+            bot.sendHtmlMessage(
+                chatId,
+                "$mention, ты сегодня уже играл, и длина твоего песюна ${dickInfo.second} см. Продолжай играть через $nextTimeString",
+                message.messageId
+            )
+            println("total :${System.currentTimeMillis()-start}ms")
+            return
+        }
+
+        // play branch
+        // песюн не может быть менее 1 см (а почему??)
+        val dickChange = (max(-5, -dickInfo.second + 1)..9).random().let {
+            // ага, 0 это 10
+            if (it == 0)
+                10
+            else
+                it
+        }
+        bot.sendHtmlMessage(
+            chatId,
+            "$mention, твой песюн ${if (dickChange > 0) "вырос на $dickChange" else "скоротился на ${-dickChange}"} см.\n" +
+                    "Теперь его длина ${dickInfo.second + dickChange} см. Продолжай играть через $nextTimeString" +
+                    if (dickInfo.second + dickChange <= KnbCommand.bet &&
+                        KnbCommand.waitingPlayers[chatId]?.first?.id == user.id && KnbCommand.waitingPlayers.remove(
+                            chatId
+                        ) != null
+                    ) {
+                        "\n\nДлина твоего песюна стала меньше ${KnbCommand.bet + 1} см, поэтому ты исключён из игры ${KnbCommand.gameTitle}"
+                    } else {
+                        ""
+                    },
+            message.messageId
+        )
+        GdDao.updateDick(chatId, user, Timestamp(nowCalendar.timeInMillis), dickInfo.second + dickChange)
+        return
+    }
+}
