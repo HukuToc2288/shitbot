81127a19ada00963a12a05bffdf3042f5d7e8417
n.chernyshev <n.chernyshev@vk.team>
2023-11-08 18:32:20 +0300

    math hotfix add timer

--- src/main/java/ru/hukutoc2288/howtoshitbot/commands/MathCommand.kt
+++ src/main/java/ru/hukutoc2288/howtoshitbot/commands/MathCommand.kt
@@ -1,7 +1,10 @@
 package ru.hukutoc2288.howtoshitbot.commands
 
+import java.util.Timer
+import java.util.TimerTask
 import org.mariuszgromada.math.mxparser.Expression
 import org.mariuszgromada.math.mxparser.License
+import org.mariuszgromada.math.mxparser.mXparser
 import org.telegram.telegrambots.meta.api.objects.Message
 import ru.hukutoc2288.howtoshitbot.bot
 import ru.hukutoc2288.howtoshitbot.utils.CommandFunction
@@ -20,19 +23,50 @@ class MathCommand : CommandFunction(
     }
 
     override fun execute(message: Message, argsLine: String) {
+        val lock = Any()
+        var shouldPrintMessage = true
+
         val expression = Expression(argsLine)
+        val cancelTimer = Timer()
+        cancelTimer.schedule(object : TimerTask(){
+            override fun run() {
+                synchronized(lock) {
+                    if (!shouldPrintMessage){
+                        return
+                    }
+                    shouldPrintMessage = false
+                    mXparser.cancelCurrentCalculation()
+                    bot.sendTextMessage(
+                        message.chatId,
+                        "Это выражение слишком сложное. Не выпендривайся - будь проще",
+                        message.messageId
+                    )
+                }
+            }
+
+        },3000)
+
         val result = expression.calculate()
-        val answerString = if (result.isNaN()) {
-            "Не могу вычислить значение. Проверь правильность выражения. Также некоторые операции могут не поддерживаться"
-        } else if (result % 1.0 == 0.0) {
-            "%d".format(result.toInt())
-        } else {
-            "%.4f".format(result)
+
+        synchronized(lock) {
+            if (!shouldPrintMessage) {
+                return
+            }
+            shouldPrintMessage = false
+            cancelTimer.cancel()
+
+            val answerString = if (result.isNaN()) {
+                "Не могу вычислить значение. Проверь правильность выражения. Также некоторые операции могут не поддерживаться"
+            } else if (result % 1.0 == 0.0) {
+                "%d".format(result.toInt())
+            } else {
+                "%.4f".format(result)
+            }
+            bot.sendTextMessage(
+                message.chatId,
+                answerString,
+                message.messageId
+            )
         }
-        bot.sendTextMessage(
-            message.chatId,
-            answerString,
-            message.messageId
-        )
     }
 }
